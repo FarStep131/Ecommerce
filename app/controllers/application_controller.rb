ecc02170f67df7923d4bd8611f37fc5efe4a8cc9
FarStep131 <farstep131@gmail.com>
2021-03-14 19:02:38 +0900

    add login authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    case resource
+    when Customer
+      root_path
+    when Admin
+      admins_root_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  namespace :admins do
+    root 'homes#top'
+  end
+  scope module: "customers" do
+    root 'homes#top'
+  end
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions',
+    registrations: 'customers/registrations'
+  }
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
+  namespace :admin do
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
